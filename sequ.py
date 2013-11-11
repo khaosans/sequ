4c0827d7fc91fc7a32b55393b705270a2e7e22ec
Souriya Khaosanga <khaosans@chandra.cs.pdx.edu>
2013-11-10 22:10:09 -0800

    finished implementation 1, howevever there are bugs which will require more research

--- sequ.py
+++ sequ.py
@@ -7,21 +7,34 @@
 #Requires Basic functionality 
 
 #Library used for system argument 
-import sys;
+from sys import argv;
 
+
+#created an error functio
 def error():
 	print 'status code 1'
 	exit(1)
 
+#Print file to screen
+def printFileToScreen(fileToPrint):
+	a = open(fileToPrint, "r")
+	line = a.readline()
+	while line:
+		print line
+		line = a.readline()
+	a.close()
+
+
 #function is used to print string inputs into integer outputs
 #in sequential order
-def buildSequence(beg,end):
+def buildSequence(beg,end,increm):
 	#Converts the strings it integers
 	begining = convertNum(beg)
-	ending = convertNum(end)+1
+	ending = convertNum(end)
 	#Prints those numbers
-	for i in range(begining,ending):
-		print i
+	while begining<=ending:
+		print begining
+		begining+=increm
 	return
 
 def convertNum(number):
@@ -30,55 +43,51 @@ def convertNum(number):
 	except ValueError:
 		return float(number)
 
-#function builds the sequence of numbers but pads with zero's
-#as defined in implementation 1 for integers
-def buildSeqPad(beg, end):
-	begLength = beg.__len__()
-	endLength = end.__len__()
 
-	if begLength >= endLength:
-		maxLength = begLength
-	else:
-		maxLength = endLength
-
-	begining = int(beg)
-	ending = int(end)
-
-	for i in range(begining,ending+1):
-		stringToPrint = str(i)
-		print stringToPrint.zfill(maxLength)
-		
-	return
 
 #function builds the sequence for floating point numbers and pads
 #the values with 0
-def buildFloatSeqPad(beg, end):
-	begLength = beg.__len__()
-	endLength = end.__len__()
+def buildSeqPad(beg, end, increment):
+	
+	
+	begining = convertNum(beg)
+	ending = convertNum(end)
+	increm = convertNum(increment)
+	
+	strBeg = str(begining)
+	strEnd = str(ending)	
+	
+	if typeOfValue(beg) == float or typeOfValue(end)==float:
+		floatBeg = float(begining)
+		floatEnd = float(ending)
+		strBeg = str(floatBeg)
+		strEnd = str(floatEnd)	
+	
+	begLength = strBeg.__len__()
+	endLength = strEnd.__len__()
 
 	if begLength >= endLength:
 		maxLength = begLength
 	else:
 		maxLength = endLength
 
-	begining = float(beg)
-	ending = float(end)
 
 	while begining <= ending:
 		stringToPrint = str(begining)
-		print stringToPrint.zfill(maxLength)
-		begining += 1
+		print stringToPrint.zfill(maxLength)	
+		begining += increm
 		
 	return
 
 #Prints the sequence of numbers for floating Point Numbers
-def buildFloatSeq(beg,end):
+def buildFloatSeq(beg,end,increment):
 	begining = convertNum(beg)
 	ending = convertNum(end)
+	increm = convertNum(increment)
 	
 	while begining <= ending:
 		print begining
-		begining += 1
+		begining += increm
 	return
 
 #From Stackoverflow how to test with python if str variable contains int float
@@ -98,9 +107,10 @@ def typeOfValue(text):
 	return str
 
 #Used for printing the seperator function
-def buildSepSeq(beg,end,character):
+def buildSepSeq(beg,end,character,increment):
 	begining = convertNum(beg)
 	ending = convertNum(end)
+	increm = convertNum(increment)
 	if begining > ending:
 		return
 	elif begining == ending:
@@ -108,10 +118,10 @@ def buildSepSeq(beg,end,character):
 		return
 	else:		
 		tempString = ''
-		while begining<=ending-1:
+		while begining<=ending-increm:
 			tempString += str(begining)
 			tempString += character
-			begining +=1
+			begining += increm
 		tempString +=str(begining)
 		print tempString
 		return
@@ -126,113 +136,143 @@ def buildFormatSeq(beg, end, inString, increment):
 		print (inString  %  begining)
 		begining+=increm
 	return
+#check for non float and int value and return a boolean value
+def isArgvNumber(beginValue):
+	lastValue = len(argv)
+	for i in range(beginValue,lastValue):
+		if typeOfValue(argv[i])==str:
+			return False
+	return True
+
+			
+		
+	
 	
 	
 
 # Main part of the program exist below this point	
 def main():
+
+			
+	#Case where no arguments exist only the call of the program
+	if len(argv)<=1:
+		return	
+	#Case where the help is called
+	if argv[1] == '--help' or argv[1] == '-h':
+		printFileToScreen("helpfile.txt")
+		return
+	#Case where the version is called
+	if argv[1] == '--version' or argv[1] == '-v':
+		printFileToScreen("versionfile.txt")	
+		return
+
 	#Test cases for format
-	if sys.argv[1]=='-f' or sys.argv[1]=='--format':
-		if len(sys.argv) == 4:
-			if typeOfValue(sys.argv[3])==int or typeOfValue(sys.argv[3])==float:
-				buildFormatSeq(1,sys.argv[3],sys.argv[2],1)
+	if argv[1]=='-f' or argv[1]=='--format':
+		if len(argv) == 4:
+			#arguments of argv beyond index 2 is a float or an int
+			if isArgvNumber(3): 
+				buildFormatSeq("1",argv[3],argv[2],"1")
 			else:
 				error()
 			
-		elif len(sys.argv) == 5: 
-			if typeOfValue(sys.argv[3])==int or typeOfValue(sys.argv[3])==float: 
-				if typeOfValue(sys.argv[4])==int or typeOfValue(sys.argv[4])==float:
-					buildFormatSeq(sys.argv[3],sys.argv[4],sys.argv[2],1)
-				else:
-					error()
+		elif len(argv) == 5: 
+			if isArgvNumber(3):
+				buildFormatSeq(argv[3],argv[4],argv[2],1)
+
 			else:
 				error()
-		elif len(sys.argv)== 6:
-			if typeOfValue(sys.argv[3])==int or typeOfValue(sys.argv[3])==float: 
-				if typeOfValue(sys.argv[4])==int or typeOfValue(sys.argv[4])==float:
-					if typeOfValue(sys.argv[5])==int or typeOfValue(sys.argv[5])==float:
-						buildFormatSeq(sys.argv[3],sys.argv[5],sys.argv[2],sys.argv[4])
-					else:
-						error()
-				else:
-					error()
+		elif len(argv)== 6:
+			if isArgvNumber(3):
+				buildFormatSeq(argv[3],argv[5],argv[2],argv[4])
 			else:
 				error()
 		else:
 			error()
-	
-	elif len(sys.argv) == 4:
-		#test cases for equal width
-		if str(sys.argv[1]) == '--equal-width' or str(sys.argv[1])=='-w': 
-			if typeOfValue(sys.argv[2])==float and typeOfValue(sys.argv[3])==float:
-				buildFloatSeqPad(sys.argv[2],sys.argv[3])
-			elif typeOfValue(sys.argv[2])==int and typeOfValue(sys.argv[3])==int:
-				buildSeqPad(sys.argv[2],sys.argv[3])
-			elif typeOfValue(sys.argv[2])==float and typeOfValue(sys.argv[3])==int:
-				buildFloatSeqPad(sys.argv[2],sys.argv[3])
-			elif typeOfValue(sys.argv[2])==int and typeOfValue(sys.argv[3]) == float:
-				buildFloatSeqPad(sys.argv[2],sys.argv[3])
+	#test cases for equal width
+	elif argv[1] == '--equal-width' or argv[1]=='-w':
+		if len(argv) == 3:
+			if isArgvNumber(2):
+				buildSeqPad("1",argv[2],"1")
 			else:
 				error()
-		#test cases for separator
-		elif sys.argv[1].startswith('--separator'):
-			separator = '--separator'
-			sepLen = separator.__len__()
-			sepString = sys.argv[1]
-			sepChar = sepString[sepLen:]
-			if typeOfValue(sys.argv[2])==float and typeOfValue(sys.argv[3])==float:
-				buildSepSeq(sys.argv[2],sys.argv[3],sepChar)
-			elif typeOfValue(sys.argv[2])==int and typeOfValue(sys.argv[3])==int:
-				buildSepSeq(sys.argv[2],sys.argv[3],sepChar)
-			elif typeOfValue(sys.argv[2])==float and typeOfValue(sys.argv[3])==int:
-				buildSepSeq(sys.argv[2],sys.argv[3],sepChar)
-			elif typeOfValue(sys.argv[2])==int and typeOfValue(sys.argv[3]) == float:
-				buildSepSeq(sys.argv[2],sys.argv[3],sepChar)
+			
+		if len(argv) ==  4:
+			if isArgvNumber(2):
+				buildSeqPad(argv[2],argv[3],"1")
 			else:
 				error()
-					
-		elif sys.argv[1].startswith('-s'):
-			separator = '-s'
-			sepLen = separator.__len__()
-			sepString = sys.argv[1]
-			sepChar = sepString[sepLen:]
-			if typeOfValue(sys.argv[2])==float and typeOfValue(sys.argv[3])==float:
-				buildSepSeq(sys.argv[2],sys.argv[3],sepChar)
-			elif typeOfValue(sys.argv[2])==int and typeOfValue(sys.argv[3])==int:
-				buildSepSeq(sys.argv[2],sys.argv[3],sepChar)
-			elif typeOfValue(sys.argv[2])==float and typeOfValue(sys.argv[3])==int:
-				buildSepSeq(sys.argv[2],sys.argv[3],sepChar)
-			elif typeOfValue(sys.argv[2])==int and typeOfValue(sys.argv[3]) == float:
-				buildSepSeq(sys.argv[2],sys.argv[3],sepChar)
+		if len(argv) == 5:
+			if isArgvNumber(2):
+				buildSeqPad(argv[2],argv[4],argv[3])
 			else:
 				error()
 
+	#test cases for separator
+	elif argv[1].startswith('--separator'):
+		separator = '--separator'
+		sepLen = separator.__len__()
+		sepString = argv[1]
+		sepChar = sepString[sepLen:]
+		if len(argv) == 3:
+			if isArgvNumber(2):
+				buildSepSeq("1",argv[2],sepChar,"1")
+			else:
+				error()
+		if len(argv) == 4:
+			if isArgvNumber(2):
+				buildSepSeq(argv[2],argv[3],sepChar,"1")
+			else:
+				error()
+		if len(argv) == 5:
+			if isArgvNumber(2):
+				buildSepSeq(argv[2],argv[4],sepChar,argv[3])
+			else:
+				error()
+	#test cases for -s					
+	elif argv[1].startswith('-s'):
+		separator = '-s'
+		sepLen = separator.__len__()
+		sepString = argv[1]
+		sepChar = sepString[sepLen:]
+
+		if len(argv) == 3:
+			if isArgvNumber(2):
+				buildSepSeq("1",argv[2],sepChar,"1")
+			else:
+				error()
+		if len(argv) == 4:
+			if isArgvNumber(2):
+				buildSepSeq(argv[2],argv[3],sepChar,"1")
+			else:
+				error()
+		if len(argv) == 5:
+			if isArgvNumber(2):
+				buildSepSeq(argv[2],argv[4],sepChar,argv[3])
+			else:
+				error()
+	#Only integers as system arguments
+	elif len(argv) == 4:
+		if isArgvNumber(2):
+			buildFloatSeq(argv[1],argv[3],argv[2])
 		else:
 			error()
+
 	#Check to see if there are exactly two arguments on 
-	elif len(sys.argv) == 3:	
-		beg = sys.argv[1]
-		end = sys.argv[2]
+	elif len(argv) == 3:	
 		#Checks for the test cases of input
-		if typeOfValue(beg) == float and typeOfValue(end) == float :
-			buildFloatSeq(beg,end)
-		elif typeOfValue(beg) == int and typeOfValue(end) == int:
-			buildSequence(beg,end)
-		elif typeOfValue(beg) == float and typeOfValue(end) == int:
-			buildFloatSeq(beg,end)
-		elif typeOfValue(beg) == int and typeOfValue(end)== float:
-			buildFloatSeq(beg,end)
+		if typeOfValue(argv[1]) == int and typeOfValue(argv[2]) == int:
+			buildSequence(argv[1],argv[2],1)
+		elif isArgvNumber(2):
+			buildFloatSeq(argv[1],argv[2],1)
 		else:
 			error()
-	elif len(sys.argv) == 2:
-		systemInput = str(sys.argv[1])
-		
-		if systemInput == '--help' or systemInput == '-h':
-			print 'helper'
-		elif systemInput == '--version' or systemInput == '-v':
-			print 'version'
+	elif len(argv) == 2:
+							
+		if isArgvNumber(1):
+			buildSequence(1,argv[1],1)
 		else:
 			error()
+	
 	else:
 		error()
 
