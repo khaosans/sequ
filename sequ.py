3dac7deca41dc1aacd8ffbce784ddb02a8ffcbca
Souriya Khaosanga <khaosans@chandra.cs.pdx.edu>
2013-11-24 01:22:28 -0800

    Implementation 2 is now working for padding with character

--- sequ.py
+++ sequ.py
@@ -1,23 +1,19 @@
 #this Python File uses the following encoding: utf-8
-#Souriya Khaosang © 2013
+#Souriya Khaosanga © 2013
 #CS300 Fall Term 2013
 
 #sequ Complience Level 0 ***CHECK***TESTED**
-#sequ Complience Level 0 ***CHECK***TESTED**
-#Requires Basic functionality 
-
+#sequ Complience Level 1 ***CHECK***TESTED**
+#sequ Compliance Level 2 ***NOT TESTED***
+ 
 #Library used for system argument 
 from sys import argv;
-import re;
 
-
-#created an error functio
 def error():
 	print 'status code 1'
 	exit(1)
 
-#Print file to screen
-def printFileToScreen(fileToPrint):
+def print_file_to_screen(fileToPrint):
 	a = open(fileToPrint, "r")
 	line = a.readline()
 	while line:
@@ -25,12 +21,18 @@ def printFileToScreen(fileToPrint):
 		line = a.readline()
 	a.close()
 
+#Convert Backslash to proper form
 def back_slash(toChange):
-	newLine = toChange.replace('\\n','\n')
-	tab = newLine.replace('\\t', '\t')
-	
-	return tab
+	backN = toChange.replace('\\n','\n')
+	backT = backN.replace('\\t', '\t')
+	backA = backT.replace('\\a','\a')
+	backF = backA.replace('\\f','\f')
+	backR = backF.replace('\\r','\r')
+	backV = backR.replace('\\v','\v')
+	backSlash = backV.replace('\\\\','\\')
+	return backSlash
 
+#Find max length of string(s) in an array
 def max_length(array):
 	maxLength = 0
 	for x in range(0,array.__len__()):
@@ -38,47 +40,40 @@ def max_length(array):
 			maxLength = array[x].__len__()
 	return maxLength
 
-
-#function is used to print string inputs into integer outputs
-#in sequential order
 def buildSequence(beg,end,increm):
 
 	#Converts the strings it integers
-	begining = convertNum(beg)
-	ending = convertNum(end)
+	beginning = convert_num(beg)
+	ending = convert_num(end)
 
 	#Prints those numbers
-	while begining<=ending:
-		print begining
-		begining+=increm
+	while beginning<=ending:
+		print beginning
+		beginning+=increm
 	return
 
-def convertNum(number):
+def convert_num(number):
 	try:
 		return int(number)
 	except ValueError:
 		return float(number)
 
-#function builds the sequence for floating point numbers and pads
-#the values with 0
-def buildSeqPad(beg, end, increment):
+def build_equal_width(beg, end, increment):
 	
-
 
 	strBuffer = []
-	begining = convertNum(beg)
-	ending = convertNum(end)
-	increm = convertNum(increment)
+	beginning = convert_num(beg)
+	ending = convert_num(end)
+	increm = convert_num(increment)
 	
-	strBeg = str(begining)
+	strBeg = str(beginning)
 	strEnd = str(ending)	
 
 
-
-	while begining <= ending:
-		stringToPrint = str(begining)
+	while beginning <= ending:
+		stringToPrint = str(beginning)
 		strBuffer.append(stringToPrint)	
-		begining += increm
+		beginning += increm
 	
 	maxLength = max_length(strBuffer)
 	
@@ -92,38 +87,35 @@ def build_char_pad(beg, end, increment, char):
 	
 
 	strBuffer = []
-	begining = convertNum(beg)
-	ending = convertNum(end)
-	increm = convertNum(increment)
+	beginning = convert_num(beg)
+	ending = convert_num(end)
+	increm = convert_num(increment)
 	
-	strBeg = str(begining)
+	strBeg = str(beginning)
 	strEnd = str(ending)	
 
-	while begining <= ending:
-		stringToPrint = str(begining)
+	while beginning <= ending:
+		stringToPrint = str(beginning)
 		strBuffer.append(stringToPrint)	
-		begining += increm
+		beginning += increm
 	
 	maxLength = max_length(strBuffer)
 	
 	for x in range(0,strBuffer.__len__()):
-		print strBuffer[x].rjust(maxLength,char)
+		print strBuffer[x].rjust(maxLength,back_slash(char))
 	return
 
-
-#Prints the sequence of numbers for floating Point Numbers
 def buildFloatSeq(beg,end,increment):
-	begining = convertNum(beg)
-	ending = convertNum(end)
-	increm = convertNum(increment)
+	beginning = convert_num(beg)
+	ending = convert_num(end)
+	increm = convert_num(increment)
 	
-	while begining <= ending:
-		print begining
-		begining += increm
+	while beginning <= ending:
+		print beginning
+		beginning += increm
 	return
 
-#From Stackoverflow how to test with python if str variable contains int float
-def typeOfValue(text):
+def type_of_value(text):
 	try:
 		int(text)
 		return int
@@ -138,134 +130,130 @@ def typeOfValue(text):
 	
 	return str
 
-#Used for printing the seperator function
-def buildSepSeq(beg,end,character,increment):
-	begining = convertNum(beg)
-	ending = convertNum(end)
-	increm = convertNum(increment)
-	if begining > ending:
+#Build implementation for separator
+def build_sep_sequence(beg,end,character,increment):
+	beginning = convert_num(beg)
+	ending = convert_num(end)
+	increm = convert_num(increment)
+	if beginning > ending:
 		return
-	elif begining == ending:
-		print begining
+	elif beginning == ending:
+		print beginning
 		return
 	else:		
 		tempString = ''
-		while begining<=ending-increm:
-			tempString +=str(begining)
+		while beginning<=ending-increm:
+			tempString +=str(beginning)
 			tempString +=str(character)
-			begining += increm
-		tempString += str(begining)
+			beginning += increm
+		tempString += str(beginning)
 		print back_slash(tempString)
 		return
 
-#Used to print the format sequence	
-def buildFormatSeq(beg, end, inString, increment):
-	begining = convertNum(beg)
-	ending = convertNum(end)
-	increm = convertNum(increment)
+def build_format_seq(beg, end, inString, increment):
+	beginning = convert_num(beg)
+	ending = convert_num(end)
+	increm = convert_num(increment)
 
-	while begining<=ending:
-		print (inString  %  begining)
-		begining+=increm
+	while beginning<=ending:
+		print (inString  %  beginning)
+		beginning+=increm
 	return
 
-#check for non float and int value and return a boolean value
-def isArgvNumber(beginValue):
+#Determines if system argument for value to be either a integer or 
+#float from beginValue to ending system argument index
+def is_argv_number(beginValue):
 	lastValue = len(argv)
 	for i in range(beginValue,lastValue):
-		if typeOfValue(argv[i])==str:
+		if type_of_value(argv[i])==str:
 			return False
 	return True
 
 			
-
-
-	
-	
-
 # Main part of the program exist below this point	
 def main():
 	
 				
-	#Case where no arguments exist only the call of the program
+	#Base case where only program is called
 	if len(argv)<=1:
 		return	
 	#Case where the help is called
 	if argv[1] == '--help' or argv[1] == '-h':
-		printFileToScreen("helpfile.txt")
+		print_file_to_screen("helpfile.txt")
 		return
 	#Case where the version is called
 	if argv[1] == '--version' or argv[1] == '-v':
-		printFileToScreen("versionfile.txt")	
+		print_file_to_screen("versionfile.txt")	
 		return
 
-	#Test cases for format
+	#Format implementation
 	if argv[1]=='-f' or argv[1]=='--format':
 		if len(argv) == 4:
 			#arguments of argv beyond index 2 is a float or an int
-			if isArgvNumber(3): 
-				buildFormatSeq("1",argv[3],argv[2],"1")
+			if is_argv_number(3): 
+				build_format_seq("1",argv[3],argv[2],"1")
 			else:
 				error()
 			
 		elif len(argv) == 5: 
-			if isArgvNumber(3):
-				buildFormatSeq(argv[3],argv[4],argv[2],1)
+			if is_argv_number(3):
+				build_format_seq(argv[3],argv[4],argv[2],1)
 
 			else:
 				error()
 		elif len(argv)== 6:
-			if isArgvNumber(3):
-				buildFormatSeq(argv[3],argv[5],argv[2],argv[4])
-			else:
-				error()
-		else:
-			error()
-	#test cases for equal width
-	elif argv[1] == '--equal-width' or argv[1]=='-w':
-		if len(argv) == 3:
-			if isArgvNumber(2):
-				buildSeqPad("1",argv[2],"1")
-			else:
-				error()
-			
-		if len(argv) ==  4:
-			if isArgvNumber(2):
-				buildSeqPad(argv[2],argv[3],"1")
-			else:
-				error()
-		if len(argv) == 5:
-			if isArgvNumber(2):
-				buildSeqPad(argv[2],argv[4],argv[3])
+			if is_argv_number(3):
+				build_format_seq(argv[3],argv[5],argv[2],argv[4])
 			else:
 				error()
 		else:
 			error()
 
-	#Cases for separator
+	#Equal Width implementation
+	elif argv[1] == '--equal-width' or argv[1]=='-w':
+		if len(argv) == 3:
+			if is_argv_number(2):
+				build_equal_width("1",argv[2],"1")
+			else:
+				error()
+			
+		elif len(argv) ==  4:
+			if is_argv_number(2):
+				build_equal_width(argv[2],argv[3],"1")
+			else:
+				error()
+		elif len(argv) == 5:
+			if is_argv_number(2):
+				build_equal_width(argv[2],argv[4],argv[3])
+			else:
+				error()
+		else:
+			error()
+
+	#Separator implementation
 	elif argv[1].startswith('--separator'):
 		separator = '--separator'
 		sepLen = separator.__len__()
 		sepString = argv[1]
 		sepChar = sepString[sepLen:]
 		if len(argv) == 3:
-			if isArgvNumber(2):
-				buildSepSeq("1",argv[2],sepChar,"1")
+			if is_argv_number(2):
+				build_sep_sequence("1",argv[2],sepChar,"1")
 			else:
 				error()
-		if len(argv) == 4:
-			if isArgvNumber(2):
-				buildSepSeq(argv[2],argv[3],sepChar,"1")
+		elif len(argv) == 4:
+			if is_argv_number(2):
+				build_sep_sequence(argv[2],argv[3],sepChar,"1")
 			else:
 				error()
-		if len(argv) == 5:
-			if isArgvNumber(2):
-				buildSepSeq(argv[2],argv[4],sepChar,argv[3])
+		elif len(argv) == 5:
+			if is_argv_number(2):
+				build_sep_sequence(argv[2],argv[4],sepChar,argv[3])
 			else:
 				error()
 		else:
 			error()
-	#Cases for -s					
+						
 	elif argv[1].startswith('-s'):
 		separator = '-s'
 		sepLen = separator.__len__()
@@ -273,104 +261,105 @@ def main():
 		sepChar = sepString[sepLen:]
 
 		if len(argv) == 3:
-			if isArgvNumber(2):
-				buildSepSeq("1",argv[2],sepChar,"1")
+			if is_argv_number(2):
+				build_sep_sequence("1",argv[2],sepChar,"1")
 			else:
 				error()
-		if len(argv) == 4:
-			if isArgvNumber(2):
-				buildSepSeq(argv[2],argv[3],sepChar,"1")
+		elif len(argv) == 4:
+			if is_argv_number(2):
+				build_sep_sequence(argv[2],argv[3],sepChar,"1")
 			else:
 				error()
-		if len(argv) == 5:
-			if isArgvNumber(2):
-				buildSepSeq(argv[2],argv[4],sepChar,argv[3])
+		elif len(argv) == 5:
+			if is_argv_number(2):
+				build_sep_sequence(argv[2],argv[4],sepChar,argv[3])
 			else:
 				error()
 		else:
 			error()
-	#Case -W and --words
+	
+	#Words implementation 
 	elif argv[1] == "-W" or argv[1] == '--words':
 		
 		sepChar = ' '
 
 		if len(argv) == 3:
-			if isArgvNumber(2):
-				buildSepSeq("1",argv[2],sepChar,"1")
+			if is_argv_number(2):
+				build_sep_sequence("1",argv[2],sepChar,"1")
 			else:
 				error()
-		if len(argv) == 4:
-			if isArgvNumber(2):
-				buildSepSeq(argv[2],argv[3],sepChar,"1")
+		elif len(argv) == 4:
+			if is_argv_number(2):
+				build_sep_sequence(argv[2],argv[3],sepChar,"1")
 			else:
 				error()
-		if len(argv) == 5:
-			if isArgvNumber(2):
-				buildSepSeq(argv[2],argv[4],sepChar,argv[3])
+		elif len(argv) == 5:
+			if is_argv_number(2):
+				build_sep_sequence(argv[2],argv[4],sepChar,argv[3])
 			else:
 				error()
 		else:
 			error()
-	#Pad with character
+
+	#Pad with character implementation
 	elif argv[1] == "-p" or argv[1] == '--pad':
 		if len(argv) == 4:
-			if isArgvNumber(3):
+			if is_argv_number(3):
 				build_char_pad('1',argv[3],'1',argv[2])
 			else:
 				error()
-		if len(argv) == 5:
-			if isArgvNumber(3):
+		elif len(argv) == 5:
+			if is_argv_number(3):
 				build_char_pad(argv[3],argv[4],'1',argv[2])
 			else:
 				error()
-		if len(argv) == 6:
-			if isArgvNumber(3):
+		elif len(argv) == 6:
+			if is_argv_number(3):
 				build_char_pad(argv[3],argv[5],argv[4],argv[2])
 			else:
 				error()
 		else:
 			error()
 
-	#Pad with spaces
+	#Pad with spaces implementation
 	elif argv[1] == "-P" or argv[1] == '--pad-spaces':
 		if len(argv) == 3:
-			if isArgvNumber(3):
+			if is_argv_number(3):
 				build_char_pad('1',argv[2],'1',' ')
 			else:
 				error()
-		if len(argv) == 4:
-			if isArgvNumber(3):
+		elif len(argv) == 4:
+			if is_argv_number(3):
 				build_char_pad(argv[2],argv[3],'1',' ')
 			else:
 				error()
-		if len(argv) == 5:
-			if isArgvNumber(3):
+		elif len(argv) == 5:
+			if is_argv_number(3):
 				build_char_pad(argv[2],argv[4],argv[3],' ')
 			else:
 				error()
 		else:
 			error()
 
-	#Only integers as system arguments
+	#Basic seq implementation 
 	elif len(argv) == 4:
-		if isArgvNumber(2):
+		if is_argv_number(2):
 			buildFloatSeq(argv[1],argv[3],argv[2])
 		else:
 			error()
 
 
-	#Check to see if there are exactly two arguments on 
 	elif len(argv) == 3:	
-		#Checks for the test cases of input
-		if typeOfValue(argv[1]) == int and typeOfValue(argv[2]) == int:
+		
+		if type_of_value(argv[1]) == int and type_of_value(argv[2]) == int:
 			buildSequence(argv[1],argv[2],1)
-		elif isArgvNumber(2):
+		elif is_argv_number(2):
 			buildFloatSeq(argv[1],argv[2],1)
 		else:
 			error()
 	elif len(argv) == 2:
 							
-		if isArgvNumber(1):
+		if is_argv_number(1):
 			buildSequence(1,argv[1],1)
 		else:
 			error()
